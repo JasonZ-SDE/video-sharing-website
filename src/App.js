bf4b8b072790d68f3d1fa9732ef12b3f73140886
Jason Zheng <hanguang.sde@gmail.com>
2023-05-14 08:01:14 -0700

    Finish the project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
+import React, { useState } from "react";
 import "./App.css";
+import youtube from "./api/youtube";
+import { Grid } from "@material-ui/core";
+import SearchBar from "./components/SearchBar";
+import VideoDetail from "./components/VideoDetail";
+import VideoList from "./components/VideoList";
 
 function App() {
-  return <div>Hello World!</div>;
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState({ id: {}, snippet: {} });
+
+  async function handleSubmit(searchTerm) {
+    const {
+      data: { items: videos },
+    } = await youtube.get("search", {
+      params: {
+        part: "snippet",
+        maxResults: 5,
+        key: "YOUR_API_KEY",
+        q: searchTerm,
+      },
+    });
+
+    setVideos(videos);
+    setSelectedVideo(videos[0]);
+  }
+
+  return (
+    <Grid styles={{ justifyContent: "center" }} container spacing={10}>
+      <Grid item xx={11}>
+        <Grid container spacing={10}>
+          <Grid item xs={12}>
+            <SearchBar onSubmit={handleSubmit} />
+          </Grid>
+          <Grid item xs={8}>
+            <VideoDetail video={selectedVideo} />
+          </Grid>
+          <Grid item xs={4}>
+            <VideoList videos={videos} onVideoSelect={setSelectedVideo} />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default App;
